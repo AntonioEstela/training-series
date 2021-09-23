eec67d9961637cf1641542e2c2fd095c721348e9
Antonio Estela <antonioestela73@gmail.com>
2021-09-22 21:11:46 -0500

    Update function using Array.reduce()

--- day-3.js
+++ day-3.js
@@ -1,32 +1,37 @@
 const input = [
-  ['a', 'b', 'c'],
-  ['c', 'd', 'f'],
-  ['d', 'f', 'a'],
+  ["a", "b", "c"],
+  ["c", "d", "f"],
+  ["d", "f", "a"],
 ];
 
+/* 
+  El input llega como una matriz
+  
+  Despues con el primer reduce lo convierto en un solo array:
+  ["a", "b", "c", "c", "d", "f", "d", "f", "a"]
+
+  Como siguiente paso uso un reduce inicializado con un objeto para empezar a contar.
+
+  si el elemento actual ya esta en el objeto, lo unico que hacemos es sumar 1,
+  de lo contrario significa que no esta, por lo tanto lo igualamos a 1:
+  {
+    a: 1,
+    b: 1,
+    c: 2,
+    d: 2,
+    f: 2,
+    g: 1,
+  }
+ */
 const getCountOfElements = (input) => {
-  const countObj = {};
-
-  input.forEach(element => {
-    element.map(item => countObj[item] ? countObj[item] += 1 : countObj[item] = 1);
-  });
-
-  return countObj;
-}
+  input.reduce((a, c) => a.concat(c), [])
+    .reduce((a, c) => {
+      a[c] ? (a[c] += 1) : (a[c] = 1);
+      return a;
+    }, {});
+};
 
 getCountOfElements(input);
-
-/*
-{
-  a: 1,
-  b: 1,
-  c: 2,
-  d: 2,
-  f: 2,
-  g: 1,
-}
-*/
-
 
 const song = ["what", "a", "wonderful", "world"];
 
